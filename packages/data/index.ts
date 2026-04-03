226e1599fa76d81dd30d58279a41c3bf8d5d6972
Nils-dev-Mertens <nilsm2950@gmail.com>
2026-04-03 02:17:01 +0200

    updated the herosection

--- packages/data/index.ts
+++ packages/data/index.ts
@@ -1,10 +1,3 @@
-export { getProjects } from './src/queries/projects';
-export type { Project } from './src/queries/projects';
-
-export { getGithubActivity } from './src/queries/github';
-export type { GithubActivity } from './src/queries/github';
-
-export { getBlogPosts } from './src/queries/blog';
-export type { BlogPost } from './src/queries/blog';
-
-export { startScheduler } from './src/scheduler';
+export * from './src/queries/projects';
+export * from './src/queries/blog';
+export * from './src/queries/github';
